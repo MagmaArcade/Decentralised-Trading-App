d59bc90ab655643aa727bdd623deafce18aabfac
gitconzo <141690721+gitconzo@users.noreply.github.com>
2023-08-28 19:40:52 +1000

    log in page start

--- asset-sphere/src/pages/Login.js
+++ asset-sphere/src/pages/Login.js
@@ -1,12 +1,21 @@
 import Navbar from "./Navbar";
+import "../css/Login.css";
 
-function App() {
+function Login() {
   return (
-     <div>
-	 <Navbar></Navbar>
-    	 <p> Login </p>
-     </div>
-);
+    <div class="Login"> {/* Use 'div' instead of 'body' */}
+      <Navbar />
+      <div class="main__container">
+        <div class="main__img--container">
+          <div class="main__img--card"><i></i></div>
+        </div>
+        <div class="main__content"></div>
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <p>paragraph paragraph paragraph paragraph paragraph</p>
+      </div>
+    </div>
+  );
 }
 
-export default App;
+export default Login;
