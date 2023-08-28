2e6e61afc7145b0c1fb96778704507174c930ff5
MagmaArcade <102445046+MagmaArcade@users.noreply.github.com>
2023-08-28 17:28:06 +1000

    update navbar

--- asset-sphere/src/pages/Navbar.js
+++ asset-sphere/src/pages/Navbar.js
@@ -1,72 +1,69 @@
+// librarys
 import { Grid } from "@mui/material";
-
 import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
 
-
+// images
 import logo from '../assets/AssetSphere_Logo.png';
 import profile from '../assets/Profile.png';
+
+// css
+import "../css/Navbar.css";
 
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="Navbar">
         <Grid 
 			container
 			direction="row"
 			justifyContent="center"
 			alignItems="center"
-  
-		container rowSpacing={1}>
-	
-	
+			container rowSpacing={1}>
+
+			<Grid item xs={3}>
+				<item> 
+					<Link to="/Home">
+						<img
+							src={logo}
+							alt={"Home"}
+							align="left"
+							style={{ width: 200, height: 50 }}
+						/>
+					</Link>
+				</item>
+			</Grid>
 			
-		<Grid item xs={3}>
-			<item> 
-				<Link to="/Home">
-					<img
-						src={logo}
-						alt={"Home"}
-						align="left"
-						style={{ width: 200, height: 50 }}
-					/>
-				</Link>
-			</item>
-		</Grid>
-		<Grid item xs={1.5}>
-			<item>
-				<Link to="/Market"> Market </Link>
-			</item>
-		</Grid>
-		<Grid item xs={1.5}>
-			<item>
-				<Link to="/Dashboard"> Dashboard </Link>
-			</item>		</Grid>
-		<Grid item xs={1.5}>
-			<item>
-				<Link to="/Wallet"> Wallet </Link>
-			</item>
-		</Grid>
-		<Grid item xs={3}>
-			<item> 
-				<Link to="/Login"> <img
-					src={profile}
-					alt={"Login"}	
-					align="right"	
-					style={{ width: 50, height: 50 }}				
-					/> 
-				</Link>
-			</item>
+			<Grid item xs={1.5}>
+				<item>
+					<Link to="/Market" > Market </Link>
+				</item>
+			</Grid>
 			
+			<Grid item xs={1.5}>
+				<item>
+					<Link to="/Dashboard"> Dashboard </Link>
+				</item>		
+			</Grid>
+			
+			<Grid item xs={1.5}>
+				<item>
+					<Link to="/Wallet"> Wallet </Link>
+				</item>
+			</Grid>
+			
+			<Grid item xs={3}>
+				<item> 
+					<Link to="/Login"> <img
+						src={profile}
+						alt={"Login"}	
+						align="right"
+						style={{ width: 50, height: 50 }}				
+						/> 
+					</Link>
+				</item>
+			</Grid>
 		</Grid>
-</Grid>
-
-
-
-
-
-
     </div>
   );
 }
