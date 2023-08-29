f2d28cc00c151e4d434827b1a695698423f128af
MagmaArcade <102445046+MagmaArcade@users.noreply.github.com>
2023-08-29 14:22:42 +1000

    market

--- asset-sphere/src/pages/Market.js
+++ asset-sphere/src/pages/Market.js
@@ -1,14 +1,30 @@
+// get navbar
 import Navbar from "./Navbar";
+
+// import images
+import market from '../assets/Market.png';
+
+// import the css syles
+import "../css/Market.css";
+
 
 function App() {
   return (
      <div>
 		<div>
-			<Navbar></Navbar >
+			<Navbar></Navbar>
 		</div>
 		
 		<div className="NavBorder">
-			<p> Wallet </p>
+			<div className="Market">
+				<p> Market </p>
+				 <img 
+				src={market}
+				alt={"market"}
+				align="center"
+				style={{ width: 1100, height: 1000 }}/>
+
+			</div>
 		</div>
      </div>
 );
