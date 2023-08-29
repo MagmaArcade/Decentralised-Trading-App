37e103a2f46efe7aa9c7762bef59bc205c480ab6
nightbroker <nicholasgustin1@gmail.com>
2023-08-29 13:53:12 +1000

    navbar working

--- asset-sphere/src/pages/Navbar.js
+++ asset-sphere/src/pages/Navbar.js
@@ -16,7 +16,7 @@ import "../css/Navbar.css";
 
 function App() {
   return (
-    <div className="Navbar">
+    <div className="navbar">
         <Grid 
 			direction="row"
 			justifyContent="center"
@@ -51,6 +51,12 @@ function App() {
 			<Grid item xs={1.5}>
 				<item>
 					<Link to="/Wallet"> Wallet </Link>
+				</item>
+			</Grid>
+
+			<Grid item xs={1.5}>
+				<item>
+					<Link to="/Trade"> Trade </Link>
 				</item>
 			</Grid>
 			
