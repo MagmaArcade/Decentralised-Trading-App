f2f0ad4c7d670dbe80e5b779db154b8420b6f818
gitconzo <141690721+gitconzo@users.noreply.github.com>
2023-09-03 03:19:05 +1000

    trade page basic functionality done
    yippie I wanna neck
    fuck margins
    fuck padding
    the graph is an image because fuck it its static and idk how to do that graph thingo

--- asset-sphere/src/pages/Trade.js
+++ asset-sphere/src/pages/Trade.js
@@ -10,36 +10,64 @@ SID:	  103865794
 // this is the Trade page. this page is view coin market history, including options to buy and sell.
 
 import React from "react";
-import "../css/Trade.css"; //import "../css/Trade.css";
+import { Link } from "react-router-dom"; // import Link from react-router-dom
+import "../css/Trade.css";
+import CoinStatistics from '../assets/CoinStatistics.png'; // import images
+
 
 function Trade() {
   return (
-  <div className="Trade">
-     <div className="main__container">
-      <div className="left__container">
-        <h1>Hello</h1>
-      </div>
-      <div className="right__container">
-        <h1>Trade Currency</h1>
-        <div className="button__row">
-          <button className="main__btn">BUY</button>
-          <button className="main__btn">SELL</button>
+    <div className="Trade">
+      <div className="main__container">
+        <div className="left__container">
+          <div className="Trade__Top">
+            <p>
+              <span className="bitcoin__text">Bitcoin</span> <span className="btc__text">BTC</span>
+            </p>
+            <button>Change Coin</button>
+          </div>
+          <div className="Trade__Middle">
+            <p>
+              <span className="coin__value">$36,021.65</span> <span className="currency">AUD</span>
+            </p>
+          </div>
+          <div className="Trade__Low">
+            <div className="date__btns__gap">
+              <button className="date__btn">1D</button>
+              <button className="date__btn">1W</button>
+              <button className="date__btn">1M</button>
+              <button className="date__btn">1Y</button>
+            </div>
+          </div>
+          <div className="Statistics">
+            <img src={CoinStatistics} alt="Stats" className="CoinStatistics"/>
+          </div>
         </div>
-        <div className="input__container">
-            <label htmlFor="amount">Amount</label>
+        <div className="right__container">
+          <h1>Trade Currency</h1>
+          <div className="button__row">
+            <button className="main__btn">BUY</button>
+            <button className="main__btn">SELL</button>
+          </div>
+          <div>
+            <label className="label" htmlFor="amount">Amount</label>
+          </div>
+          <div className="input__container">
             <input
               type="number"
               id="amount"
               name="amount"
-              placeholder="$"
+              placeholder=""
             />
-        </div>
-        <div>
-        <button className="purchase__btn">PURCHASE</button>
+            <label className="dollar-label">$</label>
+          </div>
+          <div>
+            <button className="purchase__btn">PURCHASE</button>
+          </div>
         </div>
       </div>
-     </div>
-  </div>
+    </div>
   );
-  }
-  export default Trade;
+}
+
+export default Trade;
