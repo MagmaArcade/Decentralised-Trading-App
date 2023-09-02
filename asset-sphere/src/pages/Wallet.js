4fed2f50fb2752e5c868927df31768e6f2f9cea8
MagmaArcade <102445046+MagmaArcade@users.noreply.github.com>
2023-09-02 23:39:30 +1000

    wallet

--- asset-sphere/src/pages/Wallet.js
+++ asset-sphere/src/pages/Wallet.js
@@ -13,9 +13,43 @@ import "../css/Wallet.css"; // import css styling
 
 function Wallet() {
   return (
-    <div>
-		  <p> Wallet </p>
-    </div>
+    <div className="Wallet">
+      <div className="top_container">
+        <div className="wallet_id_container">
+          <p> Wallet ID: xxxxxx </p>
+        </div>
+        <div className="button__row">
+          <button className="main__btn">Deposit</button>
+          <button className="main__btn">Withdrawl</button>
+        </div>
+      </div>
+
+     <div className="main__container">
+      <div className="left__container">
+        <h1> Trade History </h1>
+      </div>
+
+
+      <div className="right__container">
+        
+
+        <h1> Current Funds </h1>
+        
+        <div className="input-container">
+          <div>
+            <label htmlFor="coin1">Coin 1</label>
+          </div>
+          <div>
+            <label htmlFor="coin2">Coin 2</label>
+          </div>
+          <div>
+            <label htmlFor="coin3">Coin 3</label>
+          </div>
+
+          </div>
+      </div>
+     </div>
+  </div>
 );
 }
 
