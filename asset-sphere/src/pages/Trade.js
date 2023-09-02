f15b7a4dedb89091b46257c81fd735d0ca126419
gitconzo <141690721+gitconzo@users.noreply.github.com>
2023-09-02 21:49:01 +1000

    start of trade page

--- asset-sphere/src/pages/Trade.js
+++ asset-sphere/src/pages/Trade.js
@@ -1,17 +1,24 @@
-
+import React from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import Navbar from "../components/Navbar";
-
+import "../css/Trade.css";
 //import "../css/Trade.css";
 
 
 function App() {
   return (
-    <div>
-		<Navbar></Navbar >
-	
-		<p> Trade </p>
-     </div>
-);
-}
+  <div className="Trade">
+     <Navbar/>
+     <div className="main__container">
+      <div className="left__container">
+        <h1>Hello</h1>
+      </div>
+      <div className="right__container">
+        <h1>Hello</h1>
+      </div>
 
-export default App;
+     </div>
+  </div>
+  );
+  }
+  export default App;
