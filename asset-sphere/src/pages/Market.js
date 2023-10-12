2210561ec7e81dd61a98ad66d75027de75ec5b25
gitconzo <141690721+gitconzo@users.noreply.github.com>
2023-10-12 14:22:55 +1100

    table dropdown implemented
    poggers

--- asset-sphere/src/pages/Market.js
+++ asset-sphere/src/pages/Market.js
@@ -9,39 +9,67 @@ SID:	103865794
 */
 // this is the Market page. this page is used view all available coins within the market.
 
-import { Grid, TextField } from "@mui/material"; // import js elements from mui
+import React, { useState } from 'react';
+import { Grid, TextField, Select, MenuItem } from "@mui/material"; // import js elements from mui
 import "../css/Market.css"; // import the css syles
 
 // Market application
 function Market() {
+	const assets = [
+		'SwinCoin', 'ConzoCoin', 'DiamondCoin', 'NebulaCoin', 'Saturnium',
+		'FusionX', 'BitGem', 'EtherSphere', 'RipperCoin', 'LiteGem',
+		'CardanoSphere', 'LinkStar', 'Stellium', 'PolkaGem', 'CardanoSphere'
+	  ];
+
+	// State for the selected asset in dropdown
+  	const [selectedAsset, setSelectedAsset] = useState('');
+
+  	// State for the search input
+  	const [searchTerm, setSearchTerm] = useState('');
+
   return (
 	<div className="market">
 		<div className="search">
 			<Grid
 				direction="row"
-				justifyContent="right"
-				alignItems="right"
-				container spacing={2}>
-
-				<Grid item xs={4}><noscript>This is a spacer for the grid</noscript></Grid>  {/* space add an empty grid element that consume the empty grid spaces */}
-
-				<Grid item xs={2}>
-				<item> 
-					<TextField color="success" sx={{ backgroundColor: '#3b3b3b'}} focused /> {/* search bar, will post info to table to get desired search */}
-				</item>
-				</Grid>
-
-				<Grid item xs={2}>
-				<item> 
-					<button className="search-btn">Search</button>
-				</item>
-				</Grid>
+				justifyContent="space-between"
+				alignItems="center"
+				container spacing={2}
+			>
+       			{/* Left Section - Dropdown */}
+				<Grid item xs={4}>
+            		<Select
+              			value={selectedAsset}
+              			onChange={(e) => setSelectedAsset(e.target.value)}
+              			displayEmpty
+              			color="success"
+              			sx={{ backgroundColor: '#3b3b3b' }}
+            		>
+              			<MenuItem value="" disabled>
+                			Select Asset
+              			</MenuItem>
+              			{assets.map((asset) => (
+                		<MenuItem value={asset} key={asset}>
+                  			{asset}
+                		</MenuItem>
+              			))}
+            		</Select>
+          		</Grid>
+				{/* Right Section - Search */}
+				<Grid item xs={5} container justifyContent="flex-end" alignItems="center">
+          			<Grid item xs={6}>
+            			<TextField className="myTextInput" color="success" sx={{ backgroundColor: '#3b3b3b' }} focused />
+          			</Grid>
+          			<Grid item xs={4}>
+            			<button className="search-btn">Search</button>
+          			</Grid>
+        		</Grid>
 			</Grid>
 		</div>
 
 		<table>  {/* in this table element, all avaliable assets will be displayed */}
 			<tr>
-				<th>Date</th>
+				<th>Asset</th>
 				<th>Price</th>
 				<th>Change (24HR)</th>
 				<th>Volume</th>
