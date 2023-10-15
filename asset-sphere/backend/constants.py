afc96de5660e16bcb35b971e93ef0aef6055a432
gitconzo <141690721+gitconzo@users.noreply.github.com>
2023-10-15 19:36:38 +1100

    navbar.css

--- asset-sphere/backend/constants.py
+++ asset-sphere/backend/constants.py
@@ -4,4 +4,4 @@
 databasePassword = "rootroot"
 
 # Private key for the FIRST GANACHE ACCOUNT (i.e. Account with index 0)
-privateKeyGanacheAccount = "0x992c20aeee7f1523df80aade1568d31bd974a4313bea20265c5b861ef6681cca"
+privateKeyGanacheAccount = "0x7f25df951ae348136bddc21598687544c3fefb1de060376a44ede1eca61a2330"
